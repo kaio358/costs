c3d2595b31e32804e0612b67dec557044a16c37c
Kaio Luiz Ferreira <53657502+kaio358@users.noreply.github.com>
2023-08-07 17:15:31 -0300

    erro esta ocorrendo

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,17 +1,48 @@
 import { useLocation } from "react-router-dom"
+import { useState,useEffect } from "react"
+
+
 import Message from "../layout/Message"
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
+import ProjectCard from "../project/ProjectCard.js"
+
+import styles from "./Projects.module.css"
 
 function Projects(){
+    const [projects,setProject] = useState([])
+
     const location = useLocation()
     let message = ''
     if(location.state){
         message = location.state.message
     }
+    useEffect(()=>{
+        fetch("http://localhost:5000/projects",{
+            method:'GET',
+            headers:{
+                'Content-Type':'application/json'
+            }
+        }).then(resp=> resp.json()).then(data=>{
+            setProject(data)
+        }).catch(err=>console.log(err))
+    },[])
 
     return (
-    <div>
-        <h1>Meu projeto</h1>
+    <div className={styles.project_container} >
+        <div className={styles.title_container} > 
+            <h1>Meu projeto</h1>
+            <LinkButton to='/newproject' text='Criar projeto'/>
+        </div>
         {message && <Message msg={message} type="sucess"/>}
+
+        <Container customClass="start">
+            {projects.length > 0 && 
+            projects.map((project)=>{
+      
+                <ProjectCard name={project.name}/>
+            })}
+        </Container>
 
     </div>
     )
